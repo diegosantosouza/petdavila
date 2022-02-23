9ed564b5d97ecc22bc8be21680509fa04aae5772
diegosantosouza <diegosantos.s@hotmail.com>
2022-02-22 22:58:36 -0300

    fix: register route

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class RedirectIfAuthenticated
 {
